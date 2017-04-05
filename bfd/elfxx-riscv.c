17c74fd01b9d50a7ec8541c51046a6de8ef6b355
Colin Schmidt <colins@eecs.berkeley.edu>
2017-04-05 15:10:48 -0700

    RISC-V: Add hwacha jal relocation

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -870,6 +870,21 @@ static reloc_howto_type howto_table[] =
 	 0,				/* src_mask */
 	 0xffffffff,			/* dst_mask */
 	 false),			/* pcrel_offset */
+
+  /* 29-bit pc-relative Hwacha branch.  */
+  HOWTO (R_RISCV_VF_JAL,		/* type */
+	 0,				/* rightshift */
+	 2,				/* size */
+	 32,				/* bitsize */
+	 true,				/* pc_relative */
+	 0,				/* bitpos */
+	 complain_overflow_dont,	/* complain_on_overflow */
+	 bfd_elf_generic_reloc,		/* special_function */
+	 "R_RISCV_VF_JAL",		/* name */
+	 false,				/* partial_inplace */
+	 0,				/* src_mask */
+	 ENCODE_JTYPE_VIMM (-1U),	/* dst_mask */
+	 true),				/* pcrel_offset */
 };
 
 /* A mapping from BFD reloc types to RISC-V ELF reloc types.  */
@@ -931,6 +946,7 @@ static const struct elf_reloc_map riscv_reloc_map[] =
   { BFD_RELOC_RISCV_SET16, R_RISCV_SET16 },
   { BFD_RELOC_RISCV_SET32, R_RISCV_SET32 },
   { BFD_RELOC_RISCV_32_PCREL, R_RISCV_32_PCREL },
+  { BFD_RELOC_RISCV_VF_JAL, R_RISCV_VF_JAL },
 };
 
 /* Given a BFD reloc type, return a howto structure.  */
